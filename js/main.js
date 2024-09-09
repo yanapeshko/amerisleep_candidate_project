109d63e3ece8dfb031376a258c0a11a06e8c2455
Yana Peshko <yanapeshko917@gmail.com>
2024-09-09 16:38:20 -0700

    finished mobile first

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-document.querySelector(".add-to-cart").addEventListener("click", function () {
-  alert("Item added to cart!");
-});
-
 document
   .querySelector(".load-more-button")
   .addEventListener("click", function () {
-    alert("Loading more products...");
+    const productList = document.querySelector(".product-list");
+
+    const productsToClone = productList.cloneNode(true);
+
+    productList.appendChild(productsToClone);
   });
